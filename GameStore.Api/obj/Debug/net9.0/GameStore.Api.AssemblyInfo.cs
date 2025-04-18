b67b9f37daf67a0548da789352e2e4a489e2231e
Khatchi <solomon9ice2014@gmail.com>
2025-04-18 11:03:24 +0100

    UpdateGameDto implemented

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d7d6acac7d1903a8baa31859fd8c1c462522076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c400cafb8b815a4ff9f3ee6b67766c946861704d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
