8b90e67f6b111c955013ded434c23e5c514dea66
Khatchi <solomon9ice2014@gmail.com>
2025-05-04 14:20:08 +0100

    relaced the InMem data with sqlitedb using dbContext dependency injection

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -3,6 +3,7 @@ using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Entities;
 using GameStore.Api.Mapping;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Endpoints;
 
@@ -10,40 +11,25 @@ public static class GamesEndpoints
 {
     const string GetGameEndpointName = "GetGame";
 
-    private static readonly List<GameDto> games = [
-        new (
-            1,
-            "Street Fighting II",
-            "Fighting",
-            19.99M,
-            new DateOnly(1992, 7, 15)),
-            new (
-            2,
-            "Final Fantasy XIV",
-            "Roleplaying",
-            59.99M,
-            new DateOnly(2010, 9, 30)),
-            new (
-            3,
-            "FIFA 23",
-            "Sports",
-            69.99M,
-            new DateOnly(2022, 9, 27))
-    ];
-
     public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
     {
         var group = app.MapGroup("games")
                         .WithParameterValidation();
         // GET /games
-        group.MapGet("/", () => games);
+        group.MapGet("/", (GameStoreContext dbContext) =>
+            dbContext.Games.
+            Include(game => game.Genre).
+            Select(game => game.ToGameSummaryDto()).
+            AsNoTracking());
 
         // GET /games/1
-        group.MapGet("/{id}", (int id) =>
+        group.MapGet("/{id}", (int id, GameStoreContext dbContext) =>
         {
-            GameDto? game = games.Find(game => game.Id == id);
+            Game? game = dbContext.Games.Find(id);
 
-            return game is null ? Results.NotFound() : Results.Ok(game);
+            return game is null ? 
+                Results.NotFound() : 
+                Results.Ok(game.ToGameDetailsDto());
 
         })
         .WithName(GetGameEndpointName);
@@ -52,42 +38,41 @@ public static class GamesEndpoints
         group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) =>
         {
             Game game = newGame.ToEntity();
-            game.Genre = dbContext.Genres.Find(newGame.GenreId);
         
-
             dbContext.Games.Add(game);
             dbContext.SaveChanges();
  
-            return Results.CreatedAtRoute(GetGameEndpointName, 
-                new { id = game.Id}, game.ToDto());
+            return Results.CreatedAtRoute(
+                GetGameEndpointName, 
+                new { id = game.Id },
+                game.ToGameDetailsDto());
         });
 
         // PUT /games
-        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame, 
+            GameStoreContext dbContext) =>
         {
-            var index = games.FindIndex(game => game.Id == id);
+            var existingGame = dbContext.Games.Find(id);
 
-            if (index == -1)
+            if (existingGame is null)
             {
                 return Results.NotFound();
             }
 
-            games[index] = new GameDto(
-                id,
-                updatedGame.Name,
-                updatedGame.Genre,
-                updatedGame.Price,
-                updatedGame.ReleaseDate
-            );
+            dbContext.Entry(existingGame).CurrentValues.
+                SetValues(updatedGame.ToEntity(id));
+
+            dbContext.SaveChanges();
 
             return Results.NoContent();
 
         });
 
         // DELETE /games/1
-        group.MapDelete("/{id}", (int id) =>
+        group.MapDelete("/{id}", (int id, GameStoreContext dbContext) =>
         {
-            games.RemoveAll(game => game.Id == id);
+            dbContext.Games.Where(game => game.Id == id).
+                ExecuteDelete();
 
             return Results.NoContent();
         });
--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a3c633ba3c3a4ebf7b3786f455617f68f6dbb7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26eb2b45a50483cbe9b1c1ac155572b4691290ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
